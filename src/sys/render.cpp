d15fd652ab90689fd10c86c6ee1936779e33b83c
Jose Eduardo Vasquez <jose.eduardo.vasquez.oax@gmail.com>
2023-06-19 11:29:10 -0600

    se organiza mejor la clase

--- src/sys/render.cpp
+++ src/sys/render.cpp
@@ -1,18 +1,46 @@
 extern "C" {
-#include <tinyPTC/src/tinyptc.h>
+  #include <tinyPTC/src/tinyptc.h>
 }
 
+#include <memory>
 #include <sys/render.hpp>
 
 namespace ECS{
 RenderSystem_t::RenderSystem_t(uint32_t w, uint32_t h)
-: m_w{w}, m_h{h}
+: m_w{w}, m_h{h} 
 {
   ptc_open("Game", w, h);
+  m_framebuffer = std::make_unique<uint32_t[]>(m_w*m_h);
 }
-
 
 RenderSystem_t::~RenderSystem_t(){
   ptc_close();
+}
+
+// void
+// RenderSystem_t::drawSprite(){
+//     uint32_t *pscr = screen.get();
+//     const uint32_t *psp = sprite;
+//     for (uint32_t i = 0; i < 8; ++i) {
+//       for (uint32_t j = 0; j < 8; ++j) {
+//         *pscr = *psp;
+//         ++pscr;
+//         ++psp;
+//       }
+//
+//       pscr += m_w - 8;
+//     }
+// }
+
+bool
+RenderSystem_t::update() const{
+  auto screen = m_framebuffer.get();
+  
+  for (uint32_t i = 0; i < m_w*m_h; ++i) {
+    screen[i] = kR;
+  }
+  ptc_update(screen);
+  
+  return !ptc_process_events();
 }
 }
--- src/sys/render.hpp
+++ src/sys/render.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
 
 namespace ECS{
 
@@ -8,8 +9,27 @@ struct RenderSystem_t {
   explicit RenderSystem_t(uint32_t w, uint32_t h);
   ~RenderSystem_t();
 
+  bool update() const;
+  
+  //Constantes
+  static constexpr uint32_t kR = 0x00FF0000;
+  static constexpr uint32_t kG = 0x0000FF00;
+  static constexpr uint32_t kB = 0x000000FF;
+
+  static constexpr uint32_t sprite[8 * 8] = {
+    kG,kG,kG,kG,kG,kG,kG,kG,
+    kG,kB,kR,kR,kR,kR,kB,kG,
+    kG,kB,kR,kG,kG,kG,kB,kG,
+    kG,kB,kB,kR,kG,kG,kB,kG,
+    kG,kB,kB,kB,kR,kG,kB,kG,
+    kG,kB,kB,kB,kB,kR,kB,kG,
+    kG,kB,kR,kR,kR,kR,kB,kG,
+    kG,kG,kG,kG,kG,kG,kG,kG,
+  };
+
 private: 
   const uint32_t m_w{0}, m_h{0};
+  std::unique_ptr<uint32_t[]>m_framebuffer {nullptr};
 };
 
 } // namespace ECS
