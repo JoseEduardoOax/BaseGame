84267fa44b29cfbc50a6d87d66bd3fe8aa294c4f
Jose Eduardo Vasquez <jose.eduardo.vasquez.oax@gmail.com>
2023-06-21 00:32:33 -0600

    se usa lambda para pintar entity

--- src/sys/render.cpp
+++ src/sys/render.cpp
@@ -37,30 +37,22 @@ RenderSystem_t::~RenderSystem_t(){
 void
 RenderSystem_t::drawAllEntities() const{
   auto &entities { m_EntMan.getEntities() };
+  auto screen = m_framebuffer.get();
 
-  struct A{
-    A(const std::unique_ptr<uint32_t[]>& f,
-      const uint32_t& w):
-      m_framebuffer(f), m_w(w)
-    {}
-    
-    void operator()(const Entity_t &e) const {
-      auto screen = m_framebuffer.get();
-      screen += e.y * m_w + e.x;
-      auto sprite_it = begin(e.sprite);
-      for (uint32_t y = 0; y < e.h; ++y) {
-        std::copy(sprite_it, sprite_it + e.w, screen);
-        sprite_it += e.w;
-        screen += m_w;
-      }
-    };
-
-    const std::unique_ptr<uint32_t[]>& m_framebuffer;
-    const uint32_t& m_w;
+  auto getScreenXY = [&](uint32_t x, uint32_t y){
+    return screen + y*m_w + x;
+  };
+  
+  auto drawEntity = [&](const Entity_t &e) {
+    auto screen = getScreenXY(e.x, e.y);
+    auto sprite_it = begin(e.sprite);
+    for (uint32_t y = 0; y < e.h; ++y) {
+      std::copy(sprite_it, sprite_it + e.w, screen);
+      sprite_it += e.w;
+      screen += m_w;
+    }
   };
 
-  A drawEntity(m_framebuffer, m_w);
-  
   std::for_each(begin(entities), end(entities), drawEntity);
  }
 
