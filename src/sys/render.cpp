4da4623f002e73e21a8d149e6aa4aeeaa9e3e64d
Jose Eduardo Vasquez <jose.eduardo.vasquez.oax@gmail.com>
2023-06-21 00:15:53 -0600

    ejemplo de una funcion objeto o functor antes de c++11

--- src/sys/render.cpp
+++ src/sys/render.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 extern "C" {
   #include <tinyPTC/src/tinyptc.h>
 }
@@ -5,6 +6,7 @@ extern "C" {
 #include <algorithm>
 #include <memory>
 #include <sys/render.hpp>
+#include <man/entitymanager.hpp>
 
 namespace ECS{
 RenderSystem_t::RenderSystem_t(uint32_t w, uint32_t h, Entity_Manager_t& em)
@@ -19,21 +21,47 @@ RenderSystem_t::~RenderSystem_t(){
   ptc_close();
 }
 
+// void 
+// RenderSystem_t::drawEntity(const Entity_t &e) const{
+//   auto screen = m_framebuffer.get();
+//   
+//   screen += e.y * m_w + e.x;
+//   auto sprite_it = begin(e.sprite);
+//   for (uint32_t y = 0; y < e.h; ++y) {
+//     std::copy(sprite_it, sprite_it + e.w, screen);
+//     sprite_it += e.w;
+//     screen += m_w;
+//   }
+// }
+
 void
 RenderSystem_t::drawAllEntities() const{
   auto &entities { m_EntMan.getEntities() };
 
-  for(auto& e: entities){
-    auto screen = m_framebuffer.get();
-    screen += e.y*m_w + e.x;
-    auto sprite_it = begin(e.sprite);
-    for(uint32_t y=0; y<e.h; ++y){
-      std::copy(sprite_it, sprite_it + e.w, screen);
-      sprite_it += e.w;
-      screen += m_w;
-    }
+  struct A{
+    A(const std::unique_ptr<uint32_t[]>& f,
+      const uint32_t& w):
+      m_framebuffer(f), m_w(w)
+    {}
+    
+    void operator()(const Entity_t &e) const {
+      auto screen = m_framebuffer.get();
+      screen += e.y * m_w + e.x;
+      auto sprite_it = begin(e.sprite);
+      for (uint32_t y = 0; y < e.h; ++y) {
+        std::copy(sprite_it, sprite_it + e.w, screen);
+        sprite_it += e.w;
+        screen += m_w;
+      }
+    };
 
-  }
+    const std::unique_ptr<uint32_t[]>& m_framebuffer;
+    const uint32_t& m_w;
+  };
+
+  A drawEntity(m_framebuffer, m_w);
+  
+  std::for_each(begin(entities), end(entities), drawEntity);
  }
 
 bool
