f04aad34eaf0d3a4027ac6eb25c6a4acd75293b6
Jose Eduardo Vasquez <jose.eduardo.vasquez.oax@gmail.com>
2023-06-23 23:38:30 -0600

    se refactoriza entitymanager

--- src/man/entitymanager.hpp
+++ src/man/entitymanager.hpp
@@ -1,28 +1,19 @@
 #pragma once
 
-#include <cstddef>
 #include <cstdint>
 #include <vector>
+#include <util/typealiases.hpp>
+#include <util/gamecontext.hpp>
 
 namespace ECS {
 
-struct Entity_t {
-  explicit Entity_t(uint32_t _w, uint32_t _h) : w{_w}, h{_h} {
-    sprite.resize(w*h);
-  }
-
-  uint32_t x{0}, y{0};
-  uint32_t w{0}, h{0};
-  std::vector<uint32_t> sprite{};
-};
-
-struct Entity_Manager_t {
+struct Entity_Manager_t : public GameContext_t{
   using VecEntities_t = std::vector<Entity_t>;
   static constexpr std::size_t kNUMINITIALENTITIES {1000};
 
   explicit Entity_Manager_t();
   void createEntity(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t color);
-  const VecEntities_t &getEntities() const { return m_Entity; }
+  const VecEntities_t &getEntities() const override final { return m_Entity; }
 
 private:
   VecEntities_t m_Entity{};
