d725410491d3fc30152e40c9f6dd1747dba10088
Jose Eduardo Vasquez <jose.eduardo.vasquez.oax@gmail.com>
2023-06-25 22:29:59 -0600

    se agrega modificador de entidades

--- src/util/gamecontext.hpp
+++ src/util/gamecontext.hpp
@@ -4,6 +4,7 @@ namespace ECS{
 struct GameContext_t {
   virtual ~GameContext_t() = default;
   virtual const VecEntities_t& getEntities() const = 0;
+  virtual VecEntities_t& getEntites() = 0;
 };
 
 } //namespace ECS
