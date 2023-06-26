ae5be2bf776ae2126331ce6d558bc396a058475c
Jose Eduardo Vasquez <jose.eduardo.vasquez.oax@gmail.com>
2023-06-26 00:13:04 -0600

    se colocan los bytes en uint32 para colorear correctamente la imagen

--- src/cmp/entity.hpp
+++ src/cmp/entity.hpp
@@ -24,8 +24,18 @@ struct Entity_t {
     
     decodePNG(pixels, dw, dh, filevec.data(), filevec.size());
     
-    sprite.resize(pixels.size()/4);
-    std::memcpy(sprite.data(), pixels.data(), pixels.size());
+    sprite.reserve(pixels.size()/4);
+
+    for(auto p =pixels.begin(); p!=pixels.end(); p += 4){
+      uint32_t pixel =
+        static_cast<uint32_t>(*(p+0)) << 16
+      | static_cast<uint32_t>(*(p+1)) << 8
+      | static_cast<uint32_t>(*(p+2))
+      | static_cast<uint32_t>(*(p+3)) << 24;
+      sprite.push_back(pixel);
+    }
+
+    // std::memcpy(sprite.data(), pixels.data(), pixels.size());
     // sprite = std::vector<uint32_t>(pixels.begin(), pixels.end());
     w = dw; h = dh;
   }
