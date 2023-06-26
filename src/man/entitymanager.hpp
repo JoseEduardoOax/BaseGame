1d2d1bdb51fa32e833bee898414027804f449b45
Jose Eduardo Vasquez <jose.eduardo.vasquez.oax@gmail.com>
2023-06-25 22:29:25 -0600

    se agrega modificador de entidades

--- src/man/entitymanager.hpp
+++ src/man/entitymanager.hpp
@@ -13,7 +13,8 @@ struct Entity_Manager_t : public GameContext_t{
 
   explicit Entity_Manager_t();
   void createEntity(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t color);
-  const VecEntities_t &getEntities() const override final { return m_Entity; }
+  const VecEntities_t &getEntities() const override { return m_Entity; }
+  VecEntities_t& getEntites() override { return m_Entity;}
 
 private:
   VecEntities_t m_Entity{};
